e86c86f990fb39ca7905ae0889b62ef746a18f7b
Corcso <115189987+Corcso@users.noreply.github.com>
2024-10-24 19:35:51 +0100

    Added: Some load buffer code

--- SquirrelEngine/MeshDX11.cpp
+++ SquirrelEngine/MeshDX11.cpp
@@ -1,8 +1,19 @@
 #include "PCH.h"
+#ifdef DX11
 #include "MeshDX11.h"
+#include "Services.h"
 
-void SQ::MeshDX11::LoadBuffers()
-{
-	// Add buffer creation and loading here, 
-	// Talk to GraphicsDX11 Service need service locator pattern decided. 
+namespace SQ {
+	void MeshDX11::LoadBuffers()
+	{
+		// Add buffer creation and loading here, 
+		// Talk to GraphicsDX11 Service need service locator pattern decided. 
+
+		// Get graphics service which we can assume is DX11 version as we are DX11 mesh
+		GraphicsDX11* graphicsService = dynamic_cast<GraphicsDX11*>(Services::GetGraphics());
+
+		graphicsService->CreateBuffer(vertices.data(), sizeof(Vertex) * vertices.size());
+	}
 }
+
+#endif // DX11
