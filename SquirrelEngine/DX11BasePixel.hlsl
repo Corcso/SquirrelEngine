9eca53ce7a6f0c895a5df6ac4422ace4d17271fe
Corcso <115189987+Corcso@users.noreply.github.com>
2024-12-12 13:38:54 +0000

    Added: Spotlight calculation
    (De Vries, 2014)

--- SquirrelEngine/DX11BasePixel.hlsl
+++ SquirrelEngine/DX11BasePixel.hlsl
@@ -45,6 +45,17 @@ float CalculateDiffusePower(float3 pixelNormal, float3 pixelToLight)
     return max(dot(pixelToLight, pixelNormal), 0);
 }
 
+// Spotlight multiplication factor (De Vries, 2014)
+float4 CalculateSpotlightPower(float3 pixelToLight, float3 spotlightDirection, float innerCutoff, float outerCutoff)
+{
+    float angleThisPixel = acos(max(dot(pixelToLight, spotlightDirection), 0));
+    if (angleThisPixel > outerCutoff)
+        return 0;
+    if (angleThisPixel < innerCutoff)
+        return 1;
+    return (angleThisPixel - outerCutoff) / (innerCutoff - outerCutoff);
+}
+
 float4 main(Input input) : SV_TARGET{
     
     float3 ambientTotal = float3(0, 0, 0);
@@ -54,14 +65,17 @@ float4 main(Input input) : SV_TARGET{
     for (unsigned int i = 0; i < lightCount; i++)
     {
         float3 lightRayDirection = float3(0, 0, 0);
+        float spotlightMultFactor = 1;
         if (lights[i].lightType == 0)
             lightRayDirection = -normalize(lights[i].lightDirection);
         else
             lightRayDirection = normalize(lights[i].lightPosition - input.worldPosition);
         
+        if (lights[i].lightType == 2)
+            spotlightMultFactor = CalculateSpotlightPower(lightRayDirection, lights[i].lightDirection, lights[i].innerCutoffAngle, lights[i].outerCutoffAngle);
         
         ambientTotal += lights[i].ambientColor * lights[i].ambientIntensity * diffuseColor;
-        diffuseTotal += CalculateDiffusePower(input.normal, lightRayDirection) * diffuseColor;
+        diffuseTotal += CalculateDiffusePower(input.normal, lightRayDirection) * diffuseColor * spotlightMultFactor;
     }
     //return float4(input.normal / 2.0f + 0.5f, 1);
     return float4(ambientTotal + diffuseTotal, 1);
