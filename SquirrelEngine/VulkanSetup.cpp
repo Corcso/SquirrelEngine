6669e2cafc9bc6d3fadcaa23532acdb39b91610e
Corcso <115189987+Corcso@users.noreply.github.com>
2025-05-31 15:20:40 +0100

    Fixed: Vulkan Color Space
    Currently making it the same as DX11 with UNORM

--- SquirrelEngine/VulkanSetup.cpp
+++ SquirrelEngine/VulkanSetup.cpp
@@ -156,7 +156,7 @@ void SQ::VulkanSetup::CreateSwapChain(VkDevice device, VkPhysicalDevice physical
 VkSurfaceFormatKHR SQ::VulkanSetup::ChooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats) {
     // Look for SRGB 32 Bit 
     for (const auto& availableFormat : availableFormats) {
-        if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB && availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
+        if (availableFormat.format == VK_FORMAT_B8G8R8A8_UNORM && availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
             return availableFormat;
         }
     }
